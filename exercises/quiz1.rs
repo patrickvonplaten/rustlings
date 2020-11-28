adcf27c268a291e3a76fa366f18160f92d7c5d8b
Patrick von Platen <patrick.v.platen@gmail.com>
2020-11-28 12:46:22 +0100

    adapt exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,9 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn ..... {
+fn calculate_apple_price(num_apples: u32) -> u32 {
+    return if num_apples <= 40 { 2 * num_apples } else { num_apples }
+}
 
 // Don't modify this function!
 #[test]
